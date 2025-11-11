0201e801b01c848fca989c62f952260278ea2c52
polog <marcomed07@gmail.com>
2025-11-11 17:36:26 -0300

    Listas_01

--- listas_01/src/listas_01/Main.java
+++ listas_01/src/listas_01/Main.java
@@ -1,10 +1,59 @@
 package listas_01;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		List <String> list = new ArrayList<>();
+		
+		list.add("Maria");
+		list.add("Alex");
+		list.add("Bob");
+		list.add("Anna");
+		list.add(2, "Marco");
+		
+		System.out.println(list.size());
+			
+		for(String x : list) {
+		System.out.println(x);
+		}
+		
+		System.out.println("----------------------------");
+		
+		list.removeIf(x -> x.charAt(0) == 'M'); // Removeu a pela letra
+		
+		for(String x : list) {
+			System.out.println(x);
+			}
+			
+		System.out.println("----------------------------");
+			
+		System.out.println("Index of Bob " + list.indexOf("Bob"));	// mostra a possição do bob
+		
+		System.out.println("----------------------------");
+		
+		List <String> resultado = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList()); //filtra por letra
+			
+		for(String x : resultado) {
+			System.out.println(x);
+			}
+			
+		System.out.println("----------------------------");
+		
+		list.remove(0); // Removeu o Alex pelo numero da possição
+		
+		for(String x : list) {
+			System.out.println(x);
+			}
+			
+		sc.close();
 	}
 
 }
